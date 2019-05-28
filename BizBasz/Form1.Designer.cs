90ea3a549c36a9aa2ab56cd22090331c8cf076a0
CMD <kovacslaa@gmail.com>
2019-05-28 17:14:44 +0200

    really nothing

--- BizBasz/Form1.Designer.cs
+++ BizBasz/Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.btnCollect = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.btnNextGroup = new System.Windows.Forms.Button();
+            this.lblCompliance = new System.Windows.Forms.Label();
+            this.tbCompliance = new System.Windows.Forms.TextBox();
+            this.tbNet = new System.Windows.Forms.TextBox();
+            this.lnlNet = new System.Windows.Forms.Label();
+            this.tbProductName = new System.Windows.Forms.TextBox();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOpenFile
@@ -73,13 +80,15 @@
             this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(0, 95);
+            this.listView1.Location = new System.Drawing.Point(0, 206);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1074, 300);
+            this.listView1.Size = new System.Drawing.Size(1074, 189);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Descending;
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // btnNextGroup
             // 
@@ -91,11 +100,76 @@
             this.btnNextGroup.UseVisualStyleBackColor = true;
             this.btnNextGroup.Click += new System.EventHandler(this.btnNextGroup_Click);
             // 
+            // lblCompliance
+            // 
+            this.lblCompliance.AutoSize = true;
+            this.lblCompliance.Location = new System.Drawing.Point(9, 48);
+            this.lblCompliance.Name = "lblCompliance";
+            this.lblCompliance.Size = new System.Drawing.Size(59, 15);
+            this.lblCompliance.TabIndex = 5;
+            this.lblCompliance.Text = "Teljesítés";
+            // 
+            // tbCompliance
+            // 
+            this.tbCompliance.Location = new System.Drawing.Point(12, 66);
+            this.tbCompliance.Name = "tbCompliance";
+            this.tbCompliance.Size = new System.Drawing.Size(100, 20);
+            this.tbCompliance.TabIndex = 6;
+            // 
+            // tbNet
+            // 
+            this.tbNet.Location = new System.Drawing.Point(118, 66);
+            this.tbNet.Name = "tbNet";
+            this.tbNet.Size = new System.Drawing.Size(100, 20);
+            this.tbNet.TabIndex = 8;
+            // 
+            // lnlNet
+            // 
+            this.lnlNet.AutoSize = true;
+            this.lnlNet.Location = new System.Drawing.Point(115, 48);
+            this.lnlNet.Name = "lnlNet";
+            this.lnlNet.Size = new System.Drawing.Size(36, 15);
+            this.lnlNet.TabIndex = 7;
+            this.lnlNet.Text = "Nettó";
+            // 
+            // tbProductName
+            // 
+            this.tbProductName.Location = new System.Drawing.Point(224, 66);
+            this.tbProductName.Name = "tbProductName";
+            this.tbProductName.Size = new System.Drawing.Size(100, 20);
+            this.tbProductName.TabIndex = 10;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(221, 48);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(71, 15);
+            this.lblProductName.TabIndex = 9;
+            this.lblProductName.Text = "Termék név";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1074, 395);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbProductName);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.tbNet);
+            this.Controls.Add(this.lnlNet);
+            this.Controls.Add(this.tbCompliance);
+            this.Controls.Add(this.lblCompliance);
             this.Controls.Add(this.btnNextGroup);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnCollect);
@@ -118,6 +192,13 @@
         private System.Windows.Forms.Button btnCollect;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button btnNextGroup;
+        private System.Windows.Forms.Label lblCompliance;
+        private System.Windows.Forms.TextBox tbCompliance;
+        private System.Windows.Forms.TextBox tbNet;
+        private System.Windows.Forms.Label lnlNet;
+        private System.Windows.Forms.TextBox tbProductName;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BizBasz/Form1.cs
+++ BizBasz/Form1.cs
@@ -16,6 +16,7 @@ namespace BizBasz
         private bool nextGroup;
         static readonly string columns;
         private static bool mainWindowsClosed;
+      
 
         public static bool MainWindowsClosed { get => mainWindowsClosed; set => mainWindowsClosed = value; }
 
@@ -47,11 +48,13 @@ namespace BizBasz
         public Form1()
         {
             InitializeComponent();
+           
             this.FormClosing += Form1_FormClosing;
+            listView1.ColumnClick += ListView1_ColumnClick;
 
             Thread mainThread = Thread.CurrentThread;
             odt.Multiselect = false;
-            odt.Filter = "Text fájlok (*.txt)|*.txt|Pontosvesszővel tagolt fájlok (*.csv)|*.csv|Minden fájl (*.*)|*.*";
+            odt.Filter = "Pontosvesszővel tagolt fájlok (*.csv)|*.csv|Text fájlok (*.txt)|*.txt|Minden fájl (*.*)|*.*";
 
             string[] aColumn = Form1.columns.Split(';');
             foreach (string column in aColumn)
@@ -59,11 +62,15 @@ namespace BizBasz
                 ColumnHeader header = new ColumnHeader();
                 header.Text = column;
                 listView1.Columns.Add(header);
-                if (header.Text.Equals("Ügyfélkód") || header.Text.Equals("Telephely") || header.Text.Equals("Esedékes") || header.Text.Equals("Áfa Dátum"))
+                if (header.Text.Equals("Ügyfélkód") || header.Text.Equals("Telephely") || header.Text.Equals("Esedékes") || header.Text.Equals("Áfa Dátum") || header.Text.Equals("Elsz. f. szám") || header.Text.Equals("Tétel f. szám") || header.Text.Equals("Költséghely") || header.Text.Equals("Pozíciószám") || header.Text.Equals("Eladási érték") || header.Text.Equals("Engedmény"))
                 {
                     header.Width = 0;
-                }
+                }  
             }
+        }
+
+        private void ListView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
 
         }
 
@@ -80,103 +87,80 @@ namespace BizBasz
                 tbPath.Text = odt.FileName;
                 btnOpenFile.Enabled = false;
 
-
                 new Thread(() =>
                 {
                     string line;
                     char[] delimiterChars = { ';' };
-                    System.IO.StreamReader file = new System.IO.StreamReader(odt.FileName, Encoding.GetEncoding(1252));
-                    StringBuilder newFileContent = new StringBuilder();
 
-                    string prevInvoiceId = null;
-
-                    int counter = 0;
-                    while ((line = file.ReadLine()) != null)
+                    try
                     {
-                        counter++;
-                        string[] words = line.Split(delimiterChars);
-                        if (words[0].Equals("Számlaszám"))
-                        {
-                            continue;
-                        }
-                        if (counter == 2)
-                        {
-                            prevInvoiceId = words[0];
-                        }
+                        System.IO.StreamReader file = new System.IO.StreamReader(odt.FileName, Encoding.GetEncoding(1252));
+                        StringBuilder newFileContent = new StringBuilder();
 
-                        if (prevInvoiceId.Equals(words[0]))
+                        string prevInvoiceId = null;
+
+                        int counter = 0;
+                        while ((line = file.ReadLine()) != null)
                         {
-                            ControlInvoke(listView1, () =>
+                            counter++;
+                            string[] words = line.Split(delimiterChars);
+                            if (words[0].Equals("Számlaszám"))
                             {
+                                continue;
+                            }
+                            if (counter == 2)
+                            {
+                                prevInvoiceId = words[0];
+                            }
+
+                            if (prevInvoiceId.Equals(words[0]))
+                            {
+                                ControlInvoke(listView1, () =>
+                                {
                                 ListViewItem lvi = new ListViewItem(words[0]);
                                 for (int i = 1; i < words.Length; i++)
                                 {
                                     lvi.SubItems.Add(words[i]);
                                 }
                                 listView1.Items.Add(lvi);
-                            });
-
-                        }
-                        else
-                        {
-                            while (!nextGroup)
-                            {
-                                Thread.Sleep(300);
-                                if (MainWindowsClosed)
-                                {
-                                    return;
-                                }
-                            }
-
-
-                            if (nextGroup)
-                            {
-                                nextGroup = false;
-                                prevInvoiceId = words[0];
-                                ControlInvoke(listView1, () =>
-                                {
-                                    ListViewItem lvi = new ListViewItem(words[0]);
-                                    for (int i = 1; i < words.Length; i++)
-                                    {
-                                        lvi.SubItems.Add(words[i]);
-                                    }
-                                    listView1.Items.Add(lvi);
                                 });
+
+                            }
+                            else
+                            {
+                                while (!nextGroup)
+                                {
+                                    Thread.Sleep(300);
+                                    if (MainWindowsClosed)
+                                    {
+                                        return;
+                                    }
+                                }
+
+                                if (nextGroup)
+                                {
+                                    nextGroup = false;
+                                    prevInvoiceId = words[0];
+                                    ControlInvoke(listView1, () =>
+                                    {
+                                        ListViewItem lvi = new ListViewItem(words[0]);
+                                        for (int i = 1; i < words.Length; i++)
+                                        {
+                                            lvi.SubItems.Add(words[i]);
+                                        }
+                                        listView1.Items.Add(lvi);
+                                    });
+                                }
                             }
                         }
                     }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        btnOpenFile.Enabled = true;
+                    }
                 }).Start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
-
         }
 
         private void btnNextGroup_Click(object sender, EventArgs e)
@@ -194,6 +178,21 @@ namespace BizBasz
         private void Form1_Shown(object sender, EventArgs e)
         {
 
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                tbCompliance.Text = listView1.SelectedItems[0].SubItems[6].Text;
+                tbNet.Text = listView1.SelectedItems[0].SubItems[21].Text;
+                tbProductName.Text = listView1.SelectedItems[0].SubItems[12].Text;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Sort();
         }
     }
 }

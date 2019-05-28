05edc8bcd8f67dcf9b125ed96a426dca08ec1c7c
CMD <kovacslaa@gmail.com>
2019-05-28 14:14:28 +0200

    Add project files.

---
+++ BizBasz/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BizBasz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.odt = new System.Windows.Forms.OpenFileDialog();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnCollect = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnNextGroup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // odt
+            // 
+            this.odt.FileName = "openFileDialog1";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(93, 10);
+            this.tbPath.Multiline = true;
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(787, 25);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btnCollect
+            // 
+            this.btnCollect.Location = new System.Drawing.Point(12, 420);
+            this.btnCollect.Name = "btnCollect";
+            this.btnCollect.Size = new System.Drawing.Size(75, 23);
+            this.btnCollect.TabIndex = 2;
+            this.btnCollect.Text = "button1";
+            this.btnCollect.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 95);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1074, 300);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // btnNextGroup
+            // 
+            this.btnNextGroup.Location = new System.Drawing.Point(702, 54);
+            this.btnNextGroup.Name = "btnNextGroup";
+            this.btnNextGroup.Size = new System.Drawing.Size(75, 23);
+            this.btnNextGroup.TabIndex = 4;
+            this.btnNextGroup.Text = "Next";
+            this.btnNextGroup.UseVisualStyleBackColor = true;
+            this.btnNextGroup.Click += new System.EventHandler(this.btnNextGroup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 395);
+            this.Controls.Add(this.btnNextGroup);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnCollect);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog odt;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnCollect;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnNextGroup;
+    }
+}
+
---
+++ BizBasz/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BizBasz
+{
+    public partial class Form1 : Form
+    {
+        private bool nextGroup;
+        static readonly string columns;
+
+        delegate void UniversalVoidDelegate();
+
+        public static void ControlInvoke(Control control, Action function)
+        {
+            if (control.IsDisposed || control.Disposing)
+                return;
+
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new UniversalVoidDelegate(() => ControlInvoke(control, function)));
+                return;
+            }
+            function();
+        }
+
+        private void TestFunction()
+        {
+            ControlInvoke(listView1, () => listView1.Items.Add("Test"));
+        }
+
+
+
+        public void waddLvItem(string [] words)
+        {
+            ListViewItem lvi = new ListViewItem(words[0]);
+            for (int i = 1; i < words.Length; i++)
+            {
+                lvi.SubItems.Add(words[i]);
+            }
+            listView1.Items.Add(lvi);
+        }
+
+        static Form1()
+        {
+            columns = $"Számlaszám;Kézi azonosító;Ügyfélkód;Ügyfél név;Telephely;Dátum;Teljesítés;Fizetési mód;Esedékes;Könyvelés;Áfa Dátum;Elsz. f. szám;Termék név;Tétel f. szám;Költséghely;Témaszám kód;Témaszám név;Pozíciószám;Deviza;Eladási érték;Engedmény;Nettó;Áfa;Bruttó;Áfa kulcs;Egységár;Mennyiség;Mennyiségi egység";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            odt.Multiselect = false;
+            odt.Filter = "Text fájlok (*.txt)|*.txt|Pontosvesszővel tagolt fájlok (*.csv)|*.csv|Minden fájl (*.*)|*.*";
+
+            string[] aColumn = Form1.columns.Split(';');
+            foreach (string column in aColumn)
+            {
+                listView1.Columns.Add(column);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+
+            if (odt.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = odt.FileName;
+
+
+                new Thread(() =>
+                {
+                    string line;
+                    char[] delimiterChars = { ';' };
+                    System.IO.StreamReader file = new System.IO.StreamReader(odt.FileName, Encoding.GetEncoding(1252));
+                    StringBuilder newFileContent = new StringBuilder();
+
+                    bool firstrun = true;
+                    string prevInvoiceId = null;
+                    string invoiceId = null;
+                    int counter = 0;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        counter++;
+                        string[] words = line.Split(delimiterChars);
+                        if (words[0].Equals("Számlaszám"))
+                        {
+                            continue;
+                        }
+                        if (counter == 2)
+                        {
+                            prevInvoiceId = words[0];
+                        }
+
+                        if (prevInvoiceId.Equals(words[0]))
+                        {
+                            ControlInvoke(listView1, () =>
+                            {
+                                ListViewItem lvi = new ListViewItem(words[0]);
+                                for (int i = 1; i < words.Length; i++)
+                                {
+                                    lvi.SubItems.Add(words[i]);
+                                }
+                                listView1.Items.Add(lvi);
+                            });
+
+                        }
+                        else
+                        {
+                            while (!nextGroup)
+                            {
+                                    Thread.Sleep(300);
+                            }
+                           
+
+                            if (nextGroup)
+                            {
+                                nextGroup = false;
+                                prevInvoiceId = words[0];
+                                ControlInvoke(listView1, () =>
+                                {
+                                    ListViewItem lvi = new ListViewItem(words[0]);
+                                    for (int i = 1; i < words.Length; i++)
+                                    {
+                                        lvi.SubItems.Add(words[i]);
+                                    }
+                                    listView1.Items.Add(lvi);
+                                });
+                            }
+                        }
+                    }
+                    
+
+
+
+
+
+
+                }).Start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+        }
+
+        private void btnNextGroup_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            nextGroup = true;
+
+        }
+    }
+}
